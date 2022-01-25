cf808bd4c487379a15ff873d9025ad1f1fe30645
Victor Ghys <victor.ghys@student.howest.be>
2022-01-25 23:23:32 +0100

    added tile mappings for pattern symmetry to make it work properly

--- Assets/Scripts/WFCOverlapModel.cs
+++ Assets/Scripts/WFCOverlapModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -31,7 +32,13 @@ public class WFCOverlapModel : WFCModel
     private byte[,] _sample;
     // The patterns of tiles, Indexes in order: Index, Tile index in pattern
     private byte[][] _patterns;
-    private long nbOfTilesPow;
+    private long _nbOfTilesPow;
+    protected static int[] _reflectWallOffsets = { 0, 2, 0, -2 };
+    protected static int[] _reflectCornerOffsets = { 1, -1, 1, -1 };
+    //protected static int[] _rotateOffsets = { 1, 1, 1, -3 };
+    protected static int[] _rotateOffsets = { 3, -1, -1, -1 };
+    private int[] _reflectedTileIndices;
+    private int[] _rotatedTileIndices;
 
     protected override void PatternsFromSample()
     {
@@ -50,7 +57,7 @@ public class WFCOverlapModel : WFCModel
         _tiles.Clear();
         if (_input.transform.childCount < _sampleAreaWidth * _sampleAreaHeight)
         {
-            Debug.Log("Add empty tile because input is not completely filled");
+            //Debug.Log("Add empty tile because input is not completely filled");
             _tiles.Add("Empty");
             _tileDictionary.Add("Empty", 0);
             correspondingPrefabTilesList.Add(0);
@@ -87,13 +94,117 @@ public class WFCOverlapModel : WFCModel
             }
         }
         _correspondingPrefabTiles = correspondingPrefabTilesList.ToArray();
+
+        int CreateNewTile(int newPrefabIdx)
+        {
+            _tiles.Add(_tilesPrefabs[newPrefabIdx].name);
+            int newReflectTileIdx = _tiles.Count - 1;
+            int prefabIdx = _tilesPrefabs.FindIndex(tilePf => tilePf.name == _tilesPrefabs[newPrefabIdx].name);
+            correspondingPrefabTilesList.Add(prefabIdx);
+            return newReflectTileIdx;
+        }
+
+        // Pre calculate reflected and rotated versions of tiles
+        _reflectedTileIndices = new int[_tilesPrefabs.Count];
+        _rotatedTileIndices = new int[_tilesPrefabs.Count];
+        for (int i = 0; i < _tilesPrefabs.Count; i++)
+        {
+            string tile = _tilesPrefabs[i].name;
+            int tileIdx = _tiles.FindIndex(t => t == tile);
+            if (tileIdx < 0)
+            {
+                tileIdx = CreateNewTile(i);
+            }
+
+            void MakeReflectRotateIndices(string regexPattern, int[] reflectOffsets)
+            {
+                var results = Regex.Matches(tile, regexPattern).Cast<Match>().
+                    Select(x => int.Parse(x.Groups[1].Value)).ToList();
+                int rotationIdx = 0;
+                if (results.Count > 0)
+                    rotationIdx = results[0];
+
+                int newReflectPrefabIdx = i + reflectOffsets[rotationIdx];
+                int newRotatePrefabIdx = i + _rotateOffsets[rotationIdx];
+                int newReflectTileIdx = _tiles.FindIndex(t => t == _tilesPrefabs[newReflectPrefabIdx].name);
+                int newRotateTileIdx = _tiles.FindIndex(t => t == _tilesPrefabs[newRotatePrefabIdx].name);
+
+                if (newReflectTileIdx < 0)
+                {
+                    newReflectTileIdx = CreateNewTile(newReflectPrefabIdx);
+                }
+
+                if (newRotateTileIdx < 0)
+                {
+                    newRotateTileIdx = CreateNewTile(newRotatePrefabIdx);
+                }
+
+                _reflectedTileIndices[tileIdx] = newReflectTileIdx;
+                _rotatedTileIndices[tileIdx] = newRotateTileIdx;
+            }
+
+            if (Regex.IsMatch(tile, @".*Wall.*"))
+            {
+                MakeReflectRotateIndices(@".*Wall.*([0-3]+)", _reflectWallOffsets);
+            }
+            else if(Regex.IsMatch(tile, @".*Corner.*"))
+            {
+                MakeReflectRotateIndices(@".*Corner.*([0-3]+)", _reflectCornerOffsets);
+            }
+            else if(Regex.IsMatch(tile, @".*(Door|Window).*"))
+            {
+                var results = Regex.Matches(tile, @".*(Door|Window).*([0-3]+)").Cast<Match>().Select(x => int.Parse(x.Groups[2].Value)).ToList();
+                int rotationIdx = 0;
+                if (results.Count > 0)
+                    rotationIdx = results[0];
+
+                int newReflectPrefabIdx = i + _reflectWallOffsets[rotationIdx];
+                int newRotatePrefabIdx = i + _rotateOffsets[rotationIdx];
+
+                string leftPattern = @".*Left.*";
+                string rightPattern = @".*Right.*";
+                string newReflectTile = "";
+                if (Regex.IsMatch(_tilesPrefabs[newReflectPrefabIdx].name, leftPattern))
+                {
+                    newReflectTile = Regex.Replace(_tilesPrefabs[newReflectPrefabIdx].name, "Left", "Right");
+                }
+                else if (Regex.IsMatch(_tilesPrefabs[newReflectPrefabIdx].name, rightPattern))
+                {
+                    newReflectTile = Regex.Replace(_tilesPrefabs[newReflectPrefabIdx].name, "Right", "Left");
+                }
+
+                int newReflectTileIdx = _tiles.FindIndex(t => t == newReflectTile);
+                int newRotateTileIdx = _tiles.FindIndex(t => t == _tilesPrefabs[newRotatePrefabIdx].name);
+                
+                if (newReflectTileIdx < 0)
+                {
+                    _tiles.Add(newReflectTile);
+                    newReflectTileIdx = _tiles.Count - 1;
+                    int prefabIdx = _tilesPrefabs.FindIndex(tilePf => tilePf.name == newReflectTile);
+                    correspondingPrefabTilesList.Add(prefabIdx);
+                }
+                if (newRotateTileIdx < 0)
+                {
+                    newRotateTileIdx = CreateNewTile(newRotatePrefabIdx);
+                }
+
+                _reflectedTileIndices[tileIdx] = newReflectTileIdx;
+                _rotatedTileIndices[tileIdx] = newRotateTileIdx;
+            }
+            else
+            {
+                _reflectedTileIndices[tileIdx] = tileIdx;
+                _rotatedTileIndices[tileIdx] = tileIdx;
+            }
+        }
+        _correspondingPrefabTiles = correspondingPrefabTilesList.ToArray();
     }
     
     protected override void BuildPropagator()
     {
         // Look at the allowed neighours of every tile
         _nbOfTiles = _tiles.Count;
-        nbOfTilesPow = (long)Math.Pow(_nbOfTiles, _sampleSize * _sampleSize);
+        _nbOfTilesPow = (long)Math.Pow(_nbOfTiles, _sampleSize * _sampleSize);
 
         Dictionary<long, int> weights = new Dictionary<long, int>();
         List<long> ordering = new List<long>();
@@ -105,13 +216,20 @@ public class WFCOverlapModel : WFCModel
                 byte[][] ps = new byte[8][];
 
                 ps[0] = PatternFromSample(x, y);
-                ps[1] = Reflect(ps[0]);
-                ps[2] = Rotate(ps[0]);
-                ps[3] = Reflect(ps[2]);
-                ps[4] = Rotate(ps[2]);
-                ps[5] = Reflect(ps[4]);
-                ps[6] = Rotate(ps[4]);
-                ps[7] = Reflect(ps[6]);
+                ps[1] = ReflectRemapTiles(Reflect(ps[0]));
+                ps[2] = RotateRemapTiles(Rotate(ps[0]));
+                ps[3] = ReflectRemapTiles(Reflect(ps[2]));
+                ps[4] = RotateRemapTiles(Rotate(ps[2]));
+                ps[5] = ReflectRemapTiles(Reflect(ps[4]));
+                ps[6] = RotateRemapTiles(Rotate(ps[4]));
+                ps[7] = ReflectRemapTiles(Reflect(ps[6]));
+                //ps[1] = Reflect(ps[0]);
+                //ps[2] = Rotate(ps[0]);
+                //ps[3] = Reflect(ps[2]);
+                //ps[4] = Rotate(ps[2]);
+                //ps[5] = Reflect(ps[4]);
+                //ps[6] = Rotate(ps[4]);
+                //ps[7] = Reflect(ps[6]);
 
                 for (int k = 0; k < (int) _symmetry; k++)
                 {
@@ -157,6 +275,10 @@ public class WFCOverlapModel : WFCModel
                     if (Agrees(_patterns[t], _patterns[t2], XDirectionOffsets[d], YDirectionOffsets[d]))
                         list.Add(t2);
                 _propagator[d][t] = new int[list.Count];
+                if (list.Count == 0)
+                {
+                    Debug.LogWarning("doomed pattern");
+                }
                 for (int c = 0; c < list.Count; c++)
                     _propagator[d][t][c] = list[c];
             }
@@ -177,11 +299,13 @@ public class WFCOverlapModel : WFCModel
         return Pattern((dx, dy) => _sample[(x + dx) % _sampleAreaWidth, (y + dy) % _sampleAreaHeight]);
     }
 
+    // Rotate the pattern clockwise
     byte[] Rotate(byte[] p)
     {
         return Pattern((x, y) => p[_sampleSize - 1 - y + x * _sampleSize]);
     }
 
+    // Reflect the pattern horizontally
     byte[] Reflect(byte[] p)
     {
         return Pattern((x, y) => p[_sampleSize - 1 - x + y * _sampleSize]);
@@ -202,7 +326,7 @@ public class WFCOverlapModel : WFCModel
     byte[] PatternFromIndex(long ind)
     {
         long residue = ind;
-        long power = nbOfTilesPow;
+        long power = _nbOfTilesPow;
         byte[] result = new byte[_sampleSize * _sampleSize];
 
         for (int i = 0; i < result.Length; i++)
@@ -235,6 +359,34 @@ public class WFCOverlapModel : WFCModel
         return true;
     }
 
+    byte[] ReflectRemapTiles(byte[] p)
+    {
+        byte[] r = new byte[p.Length];
+        for (int i = 0; i < _sampleSize * _sampleSize; i++)
+        {
+            int reflectedIdx = _reflectedTileIndices[(int) p[i]];
+            if (reflectedIdx < 0)
+                return p;
+            r[i] = (byte) reflectedIdx;
+        }
+
+        return r;
+    }
+
+    byte[] RotateRemapTiles(byte[] p)
+    {
+        byte[] r = new byte[p.Length];
+        for (int i = 0; i < _sampleSize * _sampleSize; i++)
+        {
+            int rotatedIdx = _rotatedTileIndices[(int)p[i]];
+            if (rotatedIdx < 0)
+                return p;
+            r[i] = (byte) rotatedIdx;
+        }
+
+        return r;
+    }
+    
     protected override bool OnBoundary(int x, int y)
     {
         return x + _sampleSize > _width || y + _sampleSize > _height || x < 0 || y < 0;
